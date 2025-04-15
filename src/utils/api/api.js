12980a41c1c7b982c1d18ac58ced0077fe7cfb84
AdnanPanakkattil <muhammedadnan1247@gmail.com>
2025-04-15 11:16:33 +0530

    api

---
+++ src/utils/api/api.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+
+const baseUrl = "http://127.0.0.1:8000/"
+
+export const ApiClient = axios.create({
+    baseURL:baseUrl,
+    headers:{
+        "Content-Type":"application/json",
+        Accept:"application/json"
+    }
+})
